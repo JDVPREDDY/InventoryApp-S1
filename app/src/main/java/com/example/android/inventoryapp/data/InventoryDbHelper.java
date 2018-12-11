dba10d83d646f08deec35c65b0b24557a431dcbe
jdvpreddy <jdevivaraprasadreddy7@gmail.com>
2018-12-11 19:28:11 +0530

    updated

--- app/src/main/java/com/example/android/inventoryapp/data/InventoryDbHelper.java
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryDbHelper.java
@@ -5,7 +5,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class InventoryDbHelper extends SQLiteOpenHelper {
-    public static final String LOG_TAG = InventoryDbHelper.class.getSimpleName();
     private static final String DATABASE_NAME = "inventory.db";
     private static final int DATABASE_VERSION = 1;
 
